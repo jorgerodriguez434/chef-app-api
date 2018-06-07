2ae4f40fa26eb77744fd9719addb188abd54c415
Jorge R <jorgerodriguez434@gmail.com>
2018-06-07 14:38:31 -0500

    add heroku and travis CI

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 const express = require('express');
- const app = express();
+const app = express();
 
- const PORT = process.env.PORT || 8080;
- 
- app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-   next();
- });
- app.get('/api/*', (req, res) => {
-   res.json({ok: true});
- });
+const PORT = process.env.PORT || 8080;
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.get('/*', (req, res) => {
+  res.json({ok: true});
+});
 
- module.exports = {app};
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
 
 
 //https://thawing-ravine-79238.herokuapp.com/ | https://git.heroku.com/thawing-ravine-79238.git
