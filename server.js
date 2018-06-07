0a27e450bf6e90b58ce31b81e2f0490b890c1452
Jorge R <jorgerodriguez434@gmail.com>
2018-06-07 14:10:50 -0500

    add heroku and travis CI

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
  const app = express();
 
  const PORT = process.env.PORT || 8080;
-
+ 
+ app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+   next();
+ });
  app.get('/api/*', (req, res) => {
    res.json({ok: true});
  });
