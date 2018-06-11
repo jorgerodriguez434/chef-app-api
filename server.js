4f186cfbfb7557457b42b5cffe666dca7ef25035
Jorge R <jorgerodriguez434@gmail.com>
2018-06-10 23:24:03 -0500

    add cors and config

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
 
 app.get('/test', (req, res) => {
-  res.json({ok: true});
+  res.json({status: "success!"});
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
