c314b085e3717f6649fd5132746f0bc80f606641
Jorge R <jorgerodriguez434@gmail.com>
2018-06-12 00:37:46 -0500

    move PORT to config file

--- config.js
+++ config.js
@@ -5,3 +5,4 @@ exports.DATABASE_URL =
   "mongodb://localhost/chef-app";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-chef-app";
+exports.PORT = process.env.PORT || 8080;
