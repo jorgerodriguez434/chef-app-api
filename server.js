9979c657b2de350cad8d82d98b03c0edb49b804f
Jorge R <jorgerodriguez434@gmail.com>
2018-06-12 14:10:28 -0500

    add CORS 2

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ const { CLIENT_ORIGIN, DATABASE_URL, PORT } = require("./config");
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  next();
-});
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 
 app.use('/api/dishes', router);
 app.use(morgan("common"));
