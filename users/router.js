25dfec8405078ccc7f93392133bf224f84b1a045
Jorge R <jorgerodriguez434@gmail.com>
2018-07-09 10:47:10 -0500

    fix router

--- users/router.js
+++ users/router.js
@@ -143,7 +143,7 @@ router.post('/', jsonParser, (req, res) => {
 // we're just doing this so we have a quick way to see
 // if we're creating users. keep in mind, you can also
 // verify this in the Mongo shell.
-router.get('/', jwtAuth, (req, res) => {
+router.get('/', (req, res) => {
   return User.find()
     .then(users => res.json(users.map(user => user.serialize())))
     .catch(err => res.status(500).json({message: "internal error"}));
